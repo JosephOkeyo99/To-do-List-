6c257cf8e06891ab2519f0f35f3a3adf6bf1450b
joseph okeyo <josephokeyo99@gmail.com>
2026-01-13 14:56:37 +0300

    added close span,responsive button that adds users input

--- To-Do-List.js
+++ To-Do-List.js
@@ -1,1 +1,21 @@
-const inputbox
+const inputbox = document.getElementById('input-box');
+const listContainer =document.getElementById('list-container');
+
+function addTask(){
+  if(inputbox.value === ''){
+    alert("you must write something!"); 
+
+  }
+  else{
+    let li =document.createElement("li");
+    li.innerText = inputbox.value;
+    listContainer.appendChild(li);
+    
+    let span =document.createElement("span");
+    span.innerHTML="\u00d7"
+    li.appendChild(span);
+  }
+
+     inputbox.value = "";
+  
+}
